4de227d908c5810edce4bdbe9471461a021dbec4
yOpenSource <48642314+yOpenSource@users.noreply.github.com>
2019-08-25 01:46:52 +0530

    adding some jQuery to test its working

--- extension/extension.js
+++ extension/extension.js
@@ -1,2 +1,7 @@
 alert("hi");
 console.log("hi my dear");
+$("div").css("background-color", "yellow");
+//added jQuery
+//need to mention that in the manifest.json
+//like
+//"content_scripts":[ { "matches" : ["<all_urls>"], "js" : ["jquery-3.4.1.min.js", "extension.js" ] } ]
